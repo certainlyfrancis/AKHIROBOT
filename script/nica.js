6b588b4f6ae8aa73521e37f12fb619e657eb2ae6
Francis Loyd Raval <154427092+certainlyfrancis@users.noreply.github.com>
2024-02-10 10:34:55 +0800

    Update nica.js

--- script/nica.js
+++ script/nica.js
@@ -11,22 +11,21 @@ module.exports.config = {
 };
 
 module.exports.run = async ({ api, event, args }) => {
-  const query = args.join(" "); 
+  const query = args.join(" ");
   if (!query) {
     api.sendMessage("ℹ️ | 𝖯𝗅𝖾𝖺𝗌𝖾 𝖯𝗋𝗈𝗏𝗂𝖽𝖾 𝖲𝗈𝗆𝖾 𝖰𝗎𝖾𝗌𝗍𝗂𝗈𝗇𝗌 𝖡𝖾𝖿𝗈𝗋𝖾 𝖳𝗁𝖾 𝖢𝗈𝗆𝗆𝖺𝗇𝖽.\n\n𝗘𝗫𝗔𝗠𝗣𝗟𝗘: 𝗇𝗂𝖼𝖺 𝗐𝗁𝖺𝗍 𝗂𝗌 𝗍𝗁𝖾 𝗏𝖺𝗅𝗎𝖾 𝗈𝖿 𝖾𝖽𝗎𝖼𝖺𝗍𝗂𝗈𝗇", event.messageID, event.threadID);
-    return; 
+    return;
   }
-  
+
   api.sendMessage(`🔎 | 𝗡𝗜𝗖𝗔 𝗂𝗌 𝖺𝗇𝗌𝗐𝖾𝗋𝗂𝗇𝗀 𝗍𝗈 𝗒𝗈𝗎𝗋 𝗊𝗎𝖾𝗌𝗍𝗂𝗈𝗇.\n\n${query}`, event.threadID, () => null, event.messageID);
-  
-  try { 
-    const response = await axios.get(`https://lianeapi.onrender.com/ask/nica?key=j86bwkwo-8hako-12C&query=${encodeURIComponent(query)`);
-    api.setMessageReaction("✅", event.messageID, (err) => {}, true); 
-    api.sendMessage(response.data.message, event.threadID, () => null, event.messageID); 
-  } 
-  catch (error) {   
-    console.error(error); 
+
+  try {
+    const response = await axios.get(`https://lianeapi.onrender.com/ask/nica?key=j86bwkwo-8hako-12C&query=${encodeURIComponent(query)}`);
+    api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+    api.sendMessage(response.data.message, event.threadID, () => null, event.messageID);
+  } catch (error) {
+    console.error(error);
     api.setMessageReaction("❎", event.messageID, (err) => {}, true);
     api.sendMessage("🔴 | 𝖲𝗈𝗆𝖾𝗍𝗁𝗂𝗇𝗀 𝗐𝖾𝗇𝗍 𝗐𝗋𝗈𝗇𝗀 𝗍𝗈 𝗍𝗁𝖾 𝖠𝖯𝖨. 𝖯𝗅𝖾𝖺𝗌𝖾 𝗍𝗋𝗒 𝖺𝗀𝖺𝗂𝗇 𝗅𝖺𝗍𝖾𝗋. ", event.threadID);
-  } 
-};
+  }
+}; 
